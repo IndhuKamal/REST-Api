a0f950e8543c320b15439ad9191ffc94859a6399
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-08-30 17:53:56 +0800

    Add files via upload

---
+++ BaseClass.java
@@ -0,0 +1,51 @@
+package testCase;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import testNg.ReadExcel;
+
+public class BaseClass {
+public RemoteWebDriver driver;
+public String filename;
+@Parameters({"browser","url","username","password"})
+@BeforeMethod
+public void preConditions(String browser,String url,String username,String password) {
+	if(browser.equalsIgnoreCase("chrome")) {
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	}else if(browser.equalsIgnoreCase("Edge")) {
+		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+	}
+	driver.manage().window().maximize();
+	driver.get(url);
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.findElement(By.id("username")).sendKeys(username);
+	driver.findElement(By.id("password")).sendKeys(password);
+	driver.findElement(By.className("decorativeSubmit")).click();
+	driver.findElement(By.linkText("CRM/SFA")).click();
+	driver.findElement(By.linkText("Leads")).click();
+}
+@AfterMethod
+public void postConditions() {
+	driver.close();
+}
+@DataProvider(name="fetchValues")
+public String[][] testData() throws IOException {
+	  ReadExcel obj=new ReadExcel();
+	  String[][] leadData = obj.leadData(filename);
+	  return leadData;
+	  
+}
+}
